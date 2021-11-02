a0ef068b72ba4a371361537a5760fd1fa96ee251
lkmxmetrics <77368275+lkmxmetrics@users.noreply.github.com>
2021-11-02 15:01:10 -0600

    Update inventory.sql

--- inventory.sql
+++ inventory.sql
@@ -4,11 +4,19 @@ CREATE DATABASE IF NOT EXISTS mtg;
 
 USE mtg;
 
+## DROP TABLE inventory;
 CREATE TABLE IF NOT EXISTS inventory(
 	card_name VARCHAR(50) NOT NULL PRIMARY KEY,
 	set_name VARCHAR(10) NOT NULL,
-	rarity VARCHAR (20) NOT NULL, 
-    count INT
+	rarity VARCHAR (20) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS decklist(
+	card_name VARCHAR(50) NOT NULL,
+	deck VARCHAR(50) NOT NULL PRIMARY KEY
+);
 
+CREATE TABLE IF NOT EXISTS price(
+	card_name VARCHAR(50) NOT NULL,
+	price numeric NOT NULL
+);
